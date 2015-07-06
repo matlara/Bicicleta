f705cd2efc9116f44f6cdeda09399ad233e4a177
Matias Lara <mlara@nisum.com>
2015-07-06 16:32:39 -0300

    subiendo archivos

---
+++ works/GradleEjemplo/src/main/java/org/gradle/Main.java
@@ -0,0 +1,14 @@
+package org.gradle;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Person persona = new Person("Matias");
+		
+		System.out.println("Persona: " + persona.getName());
+
+	}
+
+}
